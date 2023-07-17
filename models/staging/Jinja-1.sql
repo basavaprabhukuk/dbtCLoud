12966b341117f38ddf47ad679285cd640ab0b662
Mr BASAVA PRABHU <prabhusidehustle@gmail.com>
2023-07-17 23:25:39 +0000

    delete

--- models/staging/Jinja-1.sql
+++
@@ -1,8 +0,0 @@
-{%- set tabletype = "orderstable" -%}
-
-
-Select ORDERID,
-       '{{tabletype}}' as table_type,
-       *
-     from 
-{{ ref('StgOrders') }}
--- models/staging/Jinja-2.sql
+++
@@ -1,8 +0,0 @@
-{%- set furnituretype = "Furniture"  -%}
-
-Select ORDERID,
-        category,
-        case when category = '{{furnituretype}}' then ORDERCOSTPRICE 
-             ELSE 0.00    end as FurnitureCategoryCostPrice  
-
-     from {{ ref('StgOrders') }}
